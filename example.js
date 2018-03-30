fbcc5fef82bedfeef2335b92b265eb9d7f0db5ae
Mateusz Zieliński <mateusz.mzielinski@gmail.com>
2018-03-30 23:25:30 +0200

    Update dependencies and release version 2.0.0

--- example.js
+++ example.js
@@ -1,60 +1,60 @@
 const wdio = require('./wdio');
 const assert = require('chai').assert;
 
-describe('Web search engines', function() {
-    this.timeout(60000);
+describe('Web search engines', function () {
+  this.timeout(60000);
 
-    // Initialize browser
-    var browser = wdio.getBrowser({
-        desiredCapabilities: {
-            browserName: 'firefox'
-        }
-    });
+  // Initialize browser
+  var browser = wdio.getBrowser({
+    desiredCapabilities: {
+      browserName: 'firefox'
+    }
+  });
 
-    // It's possible to define custom commands
-    browser.addCommand("getUrlAndTitle", function(customVar) {
-        return {
-            url: this.getUrl(),
-            title: this.getTitle(),
-            customVar: customVar
-        };
-    });
+  // It's possible to define custom commands
+  browser.addCommand('getUrlAndTitle', function (customVar) {
+    return {
+      url: this.getUrl(),
+      title: this.getTitle(),
+      customVar: customVar
+    };
+  });
 
-    // Ensure that selenium server is run locally
-    before(wdio.initSelenium);
-    
-    before(wdio.wrap(function() {
-        browser.init();
+  // Ensure that selenium server is run locally
+  before(wdio.initSelenium);
+
+  before(wdio.wrap(function () {
+    browser.init();
+  }));
+
+  after(wdio.wrap(function () {
+    browser.end();
+  }));
+
+  describe('Smoke test', function () {
+    it('Should return \'Google\' when asked about page title', wdio.wrap(function () {
+      browser.url('http://www.google.com');
+      assert.equal('Google', browser.getTitle());
     }));
+  });
 
-    after(wdio.wrap(function() {
-        browser.end();
+  describe('Add command', function () {
+    it('Should call previously defined custom command', wdio.wrap(function () {
+      browser.url('https://www.example.com/');
+      var urlAndTitle = browser.getUrlAndTitle('TestValue');
+
+      assert.equal('https://www.example.com/', urlAndTitle.url);
+      assert.equal('Example Domain', urlAndTitle.title);
+      assert.equal('TestValue', urlAndTitle.customVar);
     }));
+  });
 
-    describe('Smoke test', function () {
-        it('Should return \'Google\' when asked about page title', wdio.wrap(function () {
-            browser.url('http://www.google.com');
-            assert.equal('Google', browser.getTitle());
-        }));
+  describe('Run syntax', function () {
+    it('Should successfully run test with the \'run\' syntax', function (done) {
+      wdio.run(function () {
+        browser.url('http://www.google.com');
+        assert.equal('Google', browser.getTitle());
+      }, done)
     });
-
-    describe('Add command', function() {
-        it('Should call previously defined custom command', wdio.wrap(function () {
-            browser.url('https://www.yahoo.com/');
-            var urlAndTitle = browser.getUrlAndTitle('TestValue');
-
-            assert.equal('https://www.yahoo.com/', urlAndTitle.url);
-            assert.equal('Yahoo', urlAndTitle.title);
-            assert.equal('TestValue', urlAndTitle.customVar);
-        }));
-    });
-
-    describe('Run syntax', function() {
-        it('Should successfully run test with the \'run\' syntax', function (done) {
-            wdio.run(function() {
-                browser.url('http://www.google.com');
-                assert.equal('Google', browser.getTitle());
-            }, done)
-        });
-    });
+  });
 });
--- wdio.js
+++ wdio.js
@@ -4,120 +4,120 @@ const Future = require('fibers/future');
 const Fiber = require('fibers');
 const tcpPortUsed = require('tcp-port-used');
 
-function getAsyncCommandWrapper(fn) {
-    return function (arg1, arg2, arg3, arg4, arg5) {
-        if (!Fiber.current) {
-            throw new Error('It seems you\'ve forgotten to wrap a call to webdriver.io method into w wdio.wrap. For details see\n' +
-                'https://github.com/ziolko/wdio#errors-description');
-        }
-
-        return Future.fromPromise(fn.call(this, arg1, arg2, arg3, arg4, arg5)).wait();
+function getAsyncCommandWrapper (fn) {
+  return function (arg1, arg2, arg3, arg4, arg5) {
+    if (!Fiber.current) {
+      throw new Error('It seems you\'ve forgotten to wrap a call to webdriver.io method into w wdio.wrap. For details see\n' +
+        'https://github.com/ziolko/wdio#errors-description');
     }
+
+    return Future.fromPromise(fn.call(this, arg1, arg2, arg3, arg4, arg5)).wait();
+  }
 }
 
-function getWaitUntilCommandWrapper(fn) {
-    return getAsyncCommandWrapper(function (condition, ms, interval) {
-        return fn.call(this, function () {
-            return new Promise(function (resolve, reject) {
-                Fiber(function () {
-                    try {
-                        resolve(condition());
-                    } catch (error) {
-                        reject(error)
-                    }
-                }).run();
-            });
-        }, ms, interval);
-    });
-}
-
-exports.getBrowser = function getBrowser(options) {
-    var instance = webdriverio.remote(options);
-
-    const SYNC_COMMANDS = ['domain', '_events', '_maxListeners', 'setMaxListeners', 'emit',
-        'addListener', 'on', 'once', 'removeListener', 'removeAllListeners', 'listeners',
-        'getMaxListeners', 'listenerCount'];
-
-    const SPECIAL_COMMANDS = ['waitUntil'];
-
-    Object.keys(Object.getPrototypeOf(instance)).forEach(function (commandName) {
-        if (SYNC_COMMANDS.indexOf(commandName) === -1 && SPECIAL_COMMANDS.indexOf(commandName) === -1) {
-            instance[commandName] = getAsyncCommandWrapper(instance[commandName]);
-        }
-    });
-
-    instance.waitUntil = getWaitUntilCommandWrapper(instance.waitUntil);
-    instance.addCommand = function(name, code) {
-        instance[name] = code;
-    };
-
-    return instance;
-};
-
-exports.wrap = function wrap(code) {
-    return function (callback) {
-        if (!callback) {
-            var message = 'No callback for the wdio.wrap provided. For details see\n' +
-                'https://github.com/ziolko/wdio#errors-description';
-            throw new Error(message)
-        }
-
-        var self = this;
+function getWaitUntilCommandWrapper (fn) {
+  return getAsyncCommandWrapper(function (condition, ms, interval) {
+    return fn.call(this, function () {
+      return new Promise(function (resolve, reject) {
         Fiber(function () {
-            try {
-                code.call(self);
-                callback();
-            } catch (error) {
-                if (callback.fail) {
-                  callback.fail(error)
-                } else {
-                  callback(error);
-                }
-            }
+          try {
+            resolve(condition());
+          } catch (error) {
+            reject(error)
+          }
         }).run();
+      });
+    }, ms, interval);
+  });
+}
+
+exports.getBrowser = function getBrowser (options) {
+  var instance = webdriverio.remote(options);
+
+  const SYNC_COMMANDS = ['domain', '_events', '_maxListeners', 'setMaxListeners', 'emit',
+    'addListener', 'on', 'once', 'removeListener', 'removeAllListeners', 'listeners',
+    'getMaxListeners', 'listenerCount'];
+
+  const SPECIAL_COMMANDS = ['waitUntil'];
+
+  Object.keys(Object.getPrototypeOf(instance)).forEach(function (commandName) {
+    if (SYNC_COMMANDS.indexOf(commandName) === -1 && SPECIAL_COMMANDS.indexOf(commandName) === -1) {
+      instance[commandName] = getAsyncCommandWrapper(instance[commandName]);
     }
+  });
+
+  instance.waitUntil = getWaitUntilCommandWrapper(instance.waitUntil);
+  instance.addCommand = function (name, code) {
+    instance[name] = code;
+  };
+
+  return instance;
 };
 
-exports.run = function(code, callback) {
+exports.wrap = function wrap (code) {
+  return function (callback) {
     if (!callback) {
-        var message = 'No callback for the wdio.run provided. For details see\n' +
-            'https://github.com/ziolko/wdio#errors-description';
-        throw new Error(message)
+      var message = 'No callback for the wdio.wrap provided. For details see\n' +
+        'https://github.com/ziolko/wdio#errors-description';
+      throw new Error(message)
     }
 
-    exports.wrap(code)(callback);
+    var self = this;
+    Fiber(function () {
+      try {
+        code.call(self);
+        callback();
+      } catch (error) {
+        if (callback.fail) {
+          callback.fail(error)
+        } else {
+          callback(error);
+        }
+      }
+    }).run();
+  }
+};
+
+exports.run = function (code, callback) {
+  if (!callback) {
+    var message = 'No callback for the wdio.run provided. For details see\n' +
+      'https://github.com/ziolko/wdio#errors-description';
+    throw new Error(message)
+  }
+
+  exports.wrap(code)(callback);
 };
 
 exports.initSelenium = function (options, done) {
-    const SELENIUM_PORT = 4444;
+  const SELENIUM_PORT = 4444;
 
-    if (typeof options === 'function') {
-        done = options;
-        options = {};
-    }
+  if (typeof options === 'function') {
+    done = options;
+    options = {};
+  }
 
-    // Don't create another selenium instance if one is already running
-    // Developer can run selenium on his own so that tests run slightly faster
-    tcpPortUsed.check(SELENIUM_PORT)
-        .then(function (isPortInUse) {
-            if (isPortInUse) {
-                done();
-            } else {
-                installAndStartSelenium();
-            }
-        });
+  // Don't create another selenium instance if one is already running
+  // Developer can run selenium on his own so that tests run slightly faster
+  tcpPortUsed.check(SELENIUM_PORT)
+    .then(function (isPortInUse) {
+      if (isPortInUse) {
+        done();
+      } else {
+        installAndStartSelenium();
+      }
+    });
 
-    function installAndStartSelenium() {
-        selenium.install(options.install || {}, function (err) {
-            if (err) return done(err);
+  function installAndStartSelenium () {
+    selenium.install(options.install || {}, function (err) {
+      if (err) return done(err);
 
-            selenium.start(options.start || {}, function (err, process) {
-                done(err, process);
-            });
-        })
-    }
+      selenium.start(options.start || {}, function (err, process) {
+        done(err, process);
+      });
+    })
+  }
 };
 
 exports.endSelenium = function (process) {
-    if (process) process.kill();
+  if (process) process.kill();
 };
